653605decc9bf08e5da21432f574755c003c68a4
Joseph Emmanuel Remoto <joseph.remoto@helloconnect.org>
2026-03-30 10:57:01 +0800

    gcs to bq

---
+++ src/load/__init__.py
@@ -0,0 +1,10 @@
+"""
+Package: load
+Purpose: Modules for loading data from GCS to BigQuery.
+
+This package contains:
+- bq_schemas.py: BigQuery table schema definitions
+- bq_loader.py: Functions for loading data into BigQuery tables
+
+Part of Phase 4: BigQuery Schema & Load
+"""
